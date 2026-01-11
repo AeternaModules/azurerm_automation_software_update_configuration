7fce3f35c09df31680d1822221e08fcbd3aa5203
Daniel Allen <dan@aeternagroup.net>
2026-01-11 00:56:21 +0000

    Update to schema version v4.57.0

---
+++ main.tf
@@ -0,0 +1,97 @@
+resource "azurerm_automation_software_update_configuration" "automation_software_update_configurations" {
+  for_each = var.automation_software_update_configurations
+
+  automation_account_id    = each.value.automation_account_id
+  name                     = each.value.name
+  duration                 = each.value.duration
+  non_azure_computer_names = each.value.non_azure_computer_names
+  virtual_machine_ids      = each.value.virtual_machine_ids
+
+  schedule {
+    advanced_month_days        = each.value.schedule.advanced_month_days
+    advanced_week_days         = each.value.schedule.advanced_week_days
+    description                = each.value.schedule.description
+    expiry_time                = each.value.schedule.expiry_time
+    expiry_time_offset_minutes = each.value.schedule.expiry_time_offset_minutes
+    frequency                  = each.value.schedule.frequency
+    interval                   = each.value.schedule.interval
+    is_enabled                 = each.value.schedule.is_enabled
+    dynamic "monthly_occurrence" {
+      for_each = each.value.schedule.monthly_occurrence != null ? [each.value.schedule.monthly_occurrence] : []
+      content {
+        day        = monthly_occurrence.value.day
+        occurrence = monthly_occurrence.value.occurrence
+      }
+    }
+    next_run                  = each.value.schedule.next_run
+    next_run_offset_minutes   = each.value.schedule.next_run_offset_minutes
+    start_time                = each.value.schedule.start_time
+    start_time_offset_minutes = each.value.schedule.start_time_offset_minutes
+    time_zone                 = each.value.schedule.time_zone
+  }
+
+  dynamic "linux" {
+    for_each = each.value.linux != null ? [each.value.linux] : []
+    content {
+      classifications_included = linux.value.classifications_included
+      excluded_packages        = linux.value.excluded_packages
+      included_packages        = linux.value.included_packages
+      reboot                   = linux.value.reboot
+    }
+  }
+
+  dynamic "post_task" {
+    for_each = each.value.post_task != null ? [each.value.post_task] : []
+    content {
+      parameters = post_task.value.parameters
+      source     = post_task.value.source
+    }
+  }
+
+  dynamic "pre_task" {
+    for_each = each.value.pre_task != null ? [each.value.pre_task] : []
+    content {
+      parameters = pre_task.value.parameters
+      source     = pre_task.value.source
+    }
+  }
+
+  dynamic "target" {
+    for_each = each.value.target != null ? [each.value.target] : []
+    content {
+      dynamic "azure_query" {
+        for_each = target.value.azure_query != null ? [target.value.azure_query] : []
+        content {
+          locations  = azure_query.value.locations
+          scope      = azure_query.value.scope
+          tag_filter = azure_query.value.tag_filter
+          dynamic "tags" {
+            for_each = azure_query.value.tags != null ? [azure_query.value.tags] : []
+            content {
+              tag    = tags.value.tag
+              values = tags.value.values
+            }
+          }
+        }
+      }
+      dynamic "non_azure_query" {
+        for_each = target.value.non_azure_query != null ? [target.value.non_azure_query] : []
+        content {
+          function_alias = non_azure_query.value.function_alias
+          workspace_id   = non_azure_query.value.workspace_id
+        }
+      }
+    }
+  }
+
+  dynamic "windows" {
+    for_each = each.value.windows != null ? [each.value.windows] : []
+    content {
+      classifications_included        = windows.value.classifications_included
+      excluded_knowledge_base_numbers = windows.value.excluded_knowledge_base_numbers
+      included_knowledge_base_numbers = windows.value.included_knowledge_base_numbers
+      reboot                          = windows.value.reboot
+    }
+  }
+}
+
---
+++ variables.tf
@@ -0,0 +1,119 @@
+variable "automation_software_update_configurations" {
+  description = <<EOT
+Map of automation_software_update_configurations, attributes below
+Required:
+    - automation_account_id
+    - name
+    - schedule (block):
+        - advanced_month_days (optional)
+        - advanced_week_days (optional)
+        - description (optional)
+        - expiry_time (optional)
+        - expiry_time_offset_minutes (optional)
+        - frequency (required)
+        - interval (optional)
+        - is_enabled (optional)
+        - monthly_occurrence (optional, block):
+            - day (required)
+            - occurrence (required)
+        - next_run (optional)
+        - next_run_offset_minutes (optional)
+        - start_time (optional)
+        - start_time_offset_minutes (optional)
+        - time_zone (optional)
+Optional:
+    - duration
+    - non_azure_computer_names
+    - virtual_machine_ids
+    - linux (block):
+        - classifications_included (required)
+        - excluded_packages (optional)
+        - included_packages (optional)
+        - reboot (optional)
+    - post_task (block):
+        - parameters (optional)
+        - source (optional)
+    - pre_task (block):
+        - parameters (optional)
+        - source (optional)
+    - target (block):
+        - azure_query (optional, block):
+            - locations (optional)
+            - scope (optional)
+            - tag_filter (optional)
+            - tags (optional, block):
+                - tag (required)
+                - values (required)
+        - non_azure_query (optional, block):
+            - function_alias (optional)
+            - workspace_id (optional)
+    - windows (block):
+        - classifications_included (required)
+        - excluded_knowledge_base_numbers (optional)
+        - included_knowledge_base_numbers (optional)
+        - reboot (optional)
+EOT
+
+  type = map(object({
+    automation_account_id    = string
+    name                     = string
+    duration                 = optional(string, "PT2H")
+    non_azure_computer_names = optional(list(string))
+    virtual_machine_ids      = optional(list(string))
+    schedule = object({
+      advanced_month_days        = optional(list(number))
+      advanced_week_days         = optional(list(string))
+      description                = optional(string)
+      expiry_time                = optional(string)
+      expiry_time_offset_minutes = optional(number)
+      frequency                  = string
+      interval                   = optional(number)
+      is_enabled                 = optional(bool, true)
+      monthly_occurrence = optional(object({
+        day        = string
+        occurrence = number
+      }))
+      next_run                  = optional(string)
+      next_run_offset_minutes   = optional(number)
+      start_time                = optional(string)
+      start_time_offset_minutes = optional(number)
+      time_zone                 = optional(string, "Etc/UTC")
+    })
+    linux = optional(object({
+      classifications_included = list(string)
+      excluded_packages        = optional(list(string))
+      included_packages        = optional(list(string))
+      reboot                   = optional(string, "IfRequired")
+    }))
+    post_task = optional(object({
+      parameters = optional(map(string))
+      source     = optional(string)
+    }))
+    pre_task = optional(object({
+      parameters = optional(map(string))
+      source     = optional(string)
+    }))
+    target = optional(object({
+      azure_query = optional(object({
+        locations  = optional(list(string))
+        scope      = optional(list(string))
+        tag_filter = optional(string)
+        tags = optional(object({
+          tag    = string
+          values = list(string)
+        }))
+      }))
+      non_azure_query = optional(object({
+        function_alias = optional(string)
+        workspace_id   = optional(string)
+      }))
+    }))
+    windows = optional(object({
+      classifications_included        = list(string)
+      excluded_knowledge_base_numbers = optional(list(string))
+      included_knowledge_base_numbers = optional(list(string))
+      reboot                          = optional(string, "IfRequired")
+    }))
+  }))
+}
+
